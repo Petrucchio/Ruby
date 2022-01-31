403aab25eba1947a1721d6fd643530c7ec59de8a
Victor José Costa Farias <47372055+Petrucchio@users.noreply.github.com>
2022-01-31 16:40:31 -0300

    Make Damage
    Adionei a area de dano, a fruta de curar vida, a vida da ruby e um inimigo, tal como sua movimentação e dano causado

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class RubyController : MonoBehaviour
 {
     public float speed = 3.0f;
-
     public int maxHealth = 5;
+    public float timeInvincible = 2.0f;
     public int health { get { return currentHealth; } }
     int currentHealth;
+    bool isInvincible;
+    float invincibleTimer;
 
     Rigidbody2D rigidbody2d;
     float horizontal;
@@ -19,6 +21,7 @@ public class RubyController : MonoBehaviour
         rigidbody2d = GetComponent<Rigidbody2D>();
 
         currentHealth = maxHealth;
+
     }
 
     // Update is called once per frame
@@ -26,6 +29,13 @@ public class RubyController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
     }
     void FixedUpdate()
     {
@@ -37,6 +47,14 @@ public class RubyController : MonoBehaviour
     }
     public void ChangeHealth(int amount)
     {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
