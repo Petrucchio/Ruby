55dc98ccb312bbe7ea92f3fcd428f16243b9d4b9
Victor José Costa Farias <47372055+Petrucchio@users.noreply.github.com>
2022-02-03 21:07:40 -0300

    Animação inserida
    Após um tempo afastado por conta de doença voltei com as animações de Ruby e do robozinho

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -9,15 +9,19 @@ public class EnemyController : MonoBehaviour
     public float changeTime = 3.0f;
 
     Rigidbody2D rigidbody2D;
-
     float timer;
     int direction = 1;
 
+    Animator animator;
+
+    // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
         timer = changeTime;
+        animator = GetComponent<Animator>();
     }
+
     void Update()
     {
         timer -= Time.deltaTime;
@@ -28,21 +32,27 @@ public class EnemyController : MonoBehaviour
             timer = changeTime;
         }
     }
+
     void FixedUpdate()
     {
         Vector2 position = rigidbody2D.position;
 
         if (vertical)
         {
-            position.y = position.y + Time.deltaTime * speed * direction; ;
+            position.y = position.y + Time.deltaTime * speed * direction;
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
         }
         else
         {
-            position.x = position.x + Time.deltaTime * speed * direction; ;
+            position.x = position.x + Time.deltaTime * speed * direction;
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
         }
 
         rigidbody2D.MovePosition(position);
     }
+
     void OnCollisionEnter2D(Collision2D other)
     {
         RubyController player = other.gameObject.GetComponent<RubyController>();
