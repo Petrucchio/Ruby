4e9b3f0f42c4e283e2d1ff25fc7eaafd49840c5e
Victor José Costa Farias <47372055+Petrucchio@users.noreply.github.com>
2022-02-07 10:01:47 -0300

    NPC e Diálogo
    Foi Criado um NPC com suas animações propriamente configuradas e interação de diálogo

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -41,6 +41,18 @@ public class RubyController : MonoBehaviour
 
         Vector2 move = new Vector2(horizontal, vertical);
 
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+            if (hit.collider != null)
+            {
+                NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
+                if (character != null)
+                {
+                    character.DisplayDialog();
+                }
+            }
+        }
         if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
         {
             lookDirection.Set(move.x, move.y);
